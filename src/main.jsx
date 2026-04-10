645241bf81e17578b5fb7e21286fec8f36180e36
somsu123 <somsu123@users.noreply.github.com>
2026-04-10 13:05:30 +0530

    Main file updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import App from './App.jsx'
+import AuthContextProvider from './features/Auth/Auth.context.jsx';
+
+const queryClient = new QueryClient();
+
+
+import { ModalProvider } from './global/context/ModalContext.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient} >
+      <AuthContextProvider>
+        <ModalProvider>
+          <App />
+        </ModalProvider>
+      </AuthContextProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
